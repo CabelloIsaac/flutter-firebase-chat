5fbbb4391779244903e15988158faf209d9809e5
Isaac Cabello <cabelloisaac97@gmaill.com>
2021-03-20 21:35:06 -0500

    Select picture

--- lib/ui/screens/auth/select_avatar/select_avatar_screen.dart
+++ lib/ui/screens/auth/select_avatar/select_avatar_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase_chat/ui/widgets/title_text.dart';
+import 'package:image_picker/image_picker.dart';
 
 class SelectAvatarScreen extends StatefulWidget {
   static final String route = "/SelectAvatarScreen";
@@ -9,35 +12,56 @@ class SelectAvatarScreen extends StatefulWidget {
 }
 
 class _SelectAvatarScreenState extends State<SelectAvatarScreen> {
+  File _image;
+  final picker = ImagePicker();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Container(
-        padding: EdgeInsets.all(20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            TitleText("Selecciona tu foto de perfil."),
-            GestureDetector(
-              child: CircleAvatar(
-                radius: 100,
-                child: Icon(
-                  Icons.camera_alt,
-                  size: 75,
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              TitleText("Selecciona tu foto de perfil."),
+              GestureDetector(
+                onTap: _getImage,
+                child: CircleAvatar(
+                  radius: 100,
+                  child: _image == null
+                      ? Icon(
+                          Icons.camera_alt,
+                          size: 75,
+                        )
+                      : Container(),
+                  backgroundImage: _image == null ? null : FileImage(_image),
                 ),
               ),
-            ),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Text("Continuar"),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text("Continuar"),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  Future _getImage() async {
+    final pickedFile = await picker.getImage(source: ImageSource.gallery);
+
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+        print('No image selected.');
+      }
+    });
   }
 }
