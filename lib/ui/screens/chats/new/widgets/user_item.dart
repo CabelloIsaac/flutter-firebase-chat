36d38b677f3eda8c0d56359c5830e7156f038bd0
Isaac Cabello <cabelloisaac97@gmaill.com>
2021-03-22 19:27:07 -0500

    Clicking on user item opens new chat

--- lib/ui/screens/chats/new/widgets/user_item.dart
+++ lib/ui/screens/chats/new/widgets/user_item.dart
@@ -1,27 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase_chat/models/chat.dart';
 import 'package:flutter_firebase_chat/models/db_user.dart';
+import 'package:flutter_firebase_chat/providers/auth_provider.dart';
+import 'package:flutter_firebase_chat/providers/chats_provider.dart';
+import 'package:flutter_firebase_chat/ui/screens/chats/chat/chat_screen.dart';
+import 'package:provider/provider.dart';
 
-class UserItem extends StatelessWidget {
+class UserItem extends StatefulWidget {
   const UserItem(this.user);
   final DBUser user;
+
+  @override
+  _UserItemState createState() => _UserItemState();
+}
+
+class _UserItemState extends State<UserItem> {
+  AuthProvider _authProvider;
+  ChatsProvider _chatsProvider;
+
   @override
   Widget build(BuildContext context) {
     bool userHasValidAvatar = _userHasValidAvatar();
-    print(user.toJson());
+
+    _authProvider = Provider.of<AuthProvider>(context);
+    _chatsProvider = Provider.of<ChatsProvider>(context);
+
     return ListTile(
       leading: CircleAvatar(
         child: !userHasValidAvatar ? Icon(Icons.person) : null,
-        backgroundImage: userHasValidAvatar ? NetworkImage(user.avatar) : null,
+        backgroundImage:
+            userHasValidAvatar ? NetworkImage(widget.user.avatar) : null,
       ),
       title: Text(getUserName()),
-      onTap: () {},
+      onTap: _openChatScreen,
     );
   }
 
   bool _userHasValidAvatar() {
-    String avatar = user.avatar;
+    String avatar = widget.user.avatar;
     return avatar != null && avatar.isNotEmpty;
   }
 
-  String getUserName() => "${user.name} ${user.lastName}";
+  String getUserName() => "${widget.user.name} ${widget.user.lastName}";
+
+  void _openChatScreen() {
+    Chat chat = _createChatObject();
+    _chatsProvider.chat = chat;
+    Navigator.pushNamed(context, ChatScreen.route);
+  }
+
+  Chat _createChatObject() {
+    return Chat(
+      participants: _getListOfParticipantsIds(),
+      participantsData: _getListOfParticipantsData(),
+      type: "chat",
+    );
+  }
+
+  List<String> _getListOfParticipantsIds() {
+    return [
+      widget.user.id,
+      AuthProvider.getCurrentUserUid(),
+    ];
+  }
+
+  List<ParticipantsData> _getListOfParticipantsData() {
+    return [
+      _createParticipantsData(widget.user),
+      _createParticipantsData(_authProvider.dbUser),
+    ];
+  }
+
+  ParticipantsData _createParticipantsData(DBUser user) {
+    return ParticipantsData(
+      id: user.id,
+      avatar: user.avatar,
+      lastName: user.lastName,
+      name: user.name,
+    );
+  }
 }
