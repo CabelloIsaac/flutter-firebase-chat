4d1e19715220d3ee50e8907532a5381cbbc3e0db
Isaac Cabello <cabelloisaac97@gmaill.com>
2021-03-22 22:02:16 -0500

    Added view image in chat and hero animation

--- lib/ui/screens/chats/chat/widgets/appbar.dart
+++ lib/ui/screens/chats/chat/widgets/appbar.dart
@@ -3,6 +3,7 @@ import 'package:flutter_firebase_chat/models/chat.dart';
 import 'package:flutter_firebase_chat/providers/auth_provider.dart';
 import 'package:flutter_firebase_chat/providers/chats_provider.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_firebase_chat/ui/screens/image_detail/image_detail_screen.dart';
 
 class MyAppBar extends StatefulWidget implements PreferredSizeWidget {
   @override
@@ -22,28 +23,30 @@ class _MyAppBarState extends State<MyAppBar> {
     bool chatHasValidPicture = _chatHasValidPicture();
     String chatPicture = getChatPicture();
 
-    return AppBar(
-      titleSpacing: 0,
-      title: Row(
-        children: [
-          CircleAvatar(
-            child: !chatHasValidPicture ? Icon(Icons.person) : null,
-            backgroundImage:
-                chatHasValidPicture ? NetworkImage(chatPicture) : null,
-          ),
-          SizedBox(width: 10),
-          Text(getChatName()),
-        ],
+    return GestureDetector(
+      onTap: chatHasValidPicture
+          ? () {
+              Navigator.pushNamed(context, ImageDetailScreen.route,
+                  arguments: chatPicture);
+            }
+          : null,
+      child: AppBar(
+        titleSpacing: 0,
+        title: Row(
+          children: [
+            Hero(
+              tag: chatPicture,
+              child: CircleAvatar(
+                child: !chatHasValidPicture ? Icon(Icons.person) : null,
+                backgroundImage:
+                    chatHasValidPicture ? NetworkImage(chatPicture) : null,
+              ),
+            ),
+            SizedBox(width: 10),
+            Text(getChatName()),
+          ],
+        ),
       ),
-      actions: [
-        IconButton(
-          icon: Icon(
-            Icons.info,
-            color: Theme.of(context).primaryColor,
-          ),
-          onPressed: () {},
-        )
-      ],
     );
   }
 
